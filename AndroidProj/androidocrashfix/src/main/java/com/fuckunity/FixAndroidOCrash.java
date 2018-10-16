c1cfc02123add1a88307e84c09beb31a01078df8
Saehyun <saehyun91@neowiz.com>
2018-10-16 14:36:42 +0900

    Apply code auto indent

--- AndroidProj/androidocrashfix/src/main/java/com/fuckunity/FixAndroidOCrash.java
+++ AndroidProj/androidocrashfix/src/main/java/com/fuckunity/FixAndroidOCrash.java
@@ -1,4 +1,5 @@
 package com.fuckunity;
+
 import android.app.Application;
 import android.content.Context;
 import android.content.ServiceConnection;
@@ -17,14 +18,13 @@ import java.lang.reflect.Field;
 
 public class FixAndroidOCrash {
 
-    public  static boolean TryFix(boolean isShowLog)
-    {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            return getRunningServiceInfo( isShowLog );
-        return  true;
+    public static boolean TryFix(boolean isShowLog) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            return getRunningServiceInfo(isShowLog);
+        return true;
     }
-    private static boolean getRunningServiceInfo( boolean isShowLog ) {
 
+    private static boolean getRunningServiceInfo(boolean isShowLog) {
         try {
             Class<?> ContextImplClass = Class.forName("android.app.ContextImpl");
 
@@ -33,55 +33,52 @@ public class FixAndroidOCrash {
 
             Object packageInfo = mPackageInfoField.get(UnityPlayer.currentActivity.getBaseContext());
             Class c = mPackageInfoField.getType();
-            if(isShowLog)
-                Log.v("Test","Declared Field :" + c.getName());
+
+            if (isShowLog)
+                Log.v("Test", "Declared Field :" + c.getName());
 
             Field servicesField = c.getDeclaredField("mServices");
             servicesField.setAccessible(true);
-            ArrayMap<Context, ArrayMap<ServiceConnection, Object>>  mServices = null;
+            ArrayMap<Context, ArrayMap<ServiceConnection, Object>> mServices = null;
+
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
                 mServices = (ArrayMap<Context, ArrayMap<ServiceConnection, Object>>) servicesField.get(packageInfo);
             }
-            if( mServices == null)
-                return true;
-            if(isShowLog ) {
-                Log.d("Test", "servicesField Field :" + servicesField.getType().getName());
 
+            if (mServices == null)
+                return true;
+
+            if (isShowLog) {
+                Log.d("Test", "servicesField Field :" + servicesField.getType().getName());
                 Log.d("Test", "Size:" + mServices.size());
             }
-            for (Context context : mServices.keySet()){
-
-                for (ServiceConnection con : mServices.get(context).keySet()){
-                    if(isShowLog )
+            for (Context context : mServices.keySet()) {
+                for (ServiceConnection con : mServices.get(context).keySet()) {
+                    if (isShowLog)
                         Log.d("Test", "ContextName: " + context.toString() + "    " + "ConnectionName: " + con.toString());
-                    if(con.toString().contains("<native proxy object>") )
-                    {
+
+                    if (con.toString().contains("<native proxy object>")) {
                         UnityPlayer.currentActivity.unbindService(con);
-                        return  true;
+                        return true;
                     }
 
-                    if(isShowLog )
+                    if (isShowLog)
                         Log.d("Test", "ContextName: " + context.toString() + "    " + "ConnectionName: " + con.toString());
                 }
-
             }
-
-        }
-        catch (NoSuchFieldException e){
+        } catch (NoSuchFieldException e) {
             e.printStackTrace();
-            return  true;
-        }
-        catch (IllegalAccessException e){
+            return true;
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
-            return  true;
+            return true;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            return  true;
-        }catch ( Exception e)
-        {
+            return true;
+        } catch (Exception e) {
             e.printStackTrace();
-            return  true;
+            return true;
         }
-        return  false;
+        return false;
     }
 }
